2c3e14072a0a693068996417c540d22f7fb8ad27
jorge <aaron17ramirez@gmail.com>
2020-03-23 13:45:09 -0600

    Inicializando proyecto

---
+++ app/app.ts
@@ -0,0 +1,9 @@
+import {CreateInterventor} from "./Interventor"
+import {CreateBeneficiario} from "./Beneficiario"
+
+
+CreateInterventor("Jorge", 23, "Col predios del recreo", 33911685);
+
+//CreateBeneficiario("A","Mi super producto",20,30,100);
+
+
